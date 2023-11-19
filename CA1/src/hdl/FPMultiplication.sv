5ea59c3f7c3e646ce85c28a049b7fc4ab3a18259
Matin Bazrafshan <matinbazrafshan2003@gmail.com>
2023-11-19 21:42:14 +0330

    Last commit

--- CA1/src/hdl/FPMultiplication.sv
+++ CA1/src/hdl/FPMultiplication.sv
@@ -1,18 +1,17 @@
 module FPMultiplication(
     input [31:0] A,
     input [31:0] B,
-    output overflow,
-    output underflow,
-    output exception,
+
     output reg [31:0] result
 );
-    reg [23:0] A_Mantissa,B_Mantissa;
+    reg [23:0] A_Mantissa, B_Mantissa;
     reg [22:0] Mantissa;
     reg [47:0] Temp_Mantissa;
-    reg [7:0] A_Exponent,B_Exponent,Temp_Exponent,diff_Exponent,Exponent;
-    reg A_sign,B_sign,Sign;
+    reg [7:0] A_Exponent, B_Exponent, Temp_Exponent, diff_Exponent, Exponent;
+    reg A_sign, B_sign, Sign;
     reg [32:0] Temp;
     reg [6:0] exp_adjust;
+
     always@(*) begin
         A_Mantissa = {1'b1,A[22:0]};
         A_Exponent = A[30:23];
@@ -22,11 +21,11 @@ module FPMultiplication(
         B_Exponent = B[30:23];
         B_sign = B[31];
 
-        Temp_Exponent = A_Exponent+B_Exponent-127;
-        Temp_Mantissa = A_Mantissa*B_Mantissa;
+        Temp_Exponent = A_Exponent + B_Exponent - 127;
+        Temp_Mantissa = A_Mantissa * B_Mantissa;
         Mantissa = Temp_Mantissa[47] ? Temp_Mantissa[46:24] : Temp_Mantissa[45:23];
         Exponent = Temp_Mantissa[47] ? Temp_Exponent+1'b1 : Temp_Exponent;
-        Sign = A_sign^B_sign;
+        Sign = A_sign ^ B_sign;
         result = {Sign,Exponent,Mantissa};
         result = (A == 32'b0 | B == 32'b0) ? 32'b0 : result;
     end
--- CA1/src/hdl/PU.sv
+++ CA1/src/hdl/PU.sv
@@ -12,20 +12,14 @@ module ProcessUnit (
     wire [31:0] mult_res_0, mult_res_1, mult_res_2, mult_res_3;
     wire [31:0] sum_res_1, sum_res_2, sum_res;
 
-    wire ofm0, ofm1, ofm2, ofm3;// overflow mult
-    wire ufm0, ufm1, ufm2, ufm3;// underflow mult
-    wire exm0, exm1, exm2, exm3;// exception mult
-    FPMultiplication fpm0(x[0], w[0], ofm0, ufm0, exm0, mult_res_0);
-    FPMultiplication fpm1(x[1], w[1], ofm1, ufm1, exm1, mult_res_1);
-    FPMultiplication fpm2(x[2], w[2], ofm2, ufm2, exm2, mult_res_2);
-    FPMultiplication fpm3(x[3], w[3], ofm3, ufm3, exm3, mult_res_3);
+    FPMultiplication fpm0(x[0], w[0], mult_res_0);
+    FPMultiplication fpm1(x[1], w[1], mult_res_1);
+    FPMultiplication fpm2(x[2], w[2], mult_res_2);
+    FPMultiplication fpm3(x[3], w[3], mult_res_3);
 
-    wire ofa0, ofa1, ofa2;// overflow add
-    wire ufa0, ufa1, ufa2;// underflow add
-    wire exa0, exa1, exa2;// exception add
-    FPAddition fpa0(mult_reg[0], mult_reg[1], ofa0, ufa0, exa0, sum_res_1);
-    FPAddition fpa1(mult_reg[2], mult_reg[3], ofa1, ufa1, exa1, sum_res_2);
-    FPAddition fpa2(sum_res_1, sum_res_2, ofa2, ufa2, exa2, sum_res);
+    FPAddition fpa0(mult_reg[0], mult_reg[1], sum_res_1);
+    FPAddition fpa1(mult_reg[2], mult_reg[3], sum_res_2);
+    FPAddition fpa2(sum_res_1, sum_res_2, sum_res);
 
     always @(clock) begin
         if(load_mult) begin
